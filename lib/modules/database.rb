46c01e28a89d8d00553d130131189ce95f805dd6
z64 <zachnowicki@gmail.com>
2016-09-04 15:15:12 +0000

    add database.rb

---
+++ lib/modules/database.rb
@@ -0,0 +1,13 @@
+module DrawBot
+  module Database
+    # Connect to database
+    DB = Sequel.connect('sqlite://data/drawbot.db')
+
+    # Load migrations
+    Sequel.extension :migration
+    Sequel::Migrator.run(DB, 'lib/modules/database/migrations')
+
+    # Load models
+    Dir['lib/modules/database/*.rb'].each { |mod| load mod }
+  end
+end
