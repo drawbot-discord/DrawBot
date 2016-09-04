a5553768198ee05e5f91ddc6aefbd6be96f5a110
z64 <zachnowicki@gmail.com>
2016-09-04 15:52:58 +0000

    add docs

--- lib/modules/database/response.rb
+++ lib/modules/database/response.rb
@@ -1,5 +1,7 @@
 module DrawBot
   module Database
+    # A collection of chat reponses based
+    # off a certain key.
     class Response < Sequel::Model
       def after_create
         Discordrb::LOGGER.info "created response: #{inspect}"
