2de35bc983b0d01fbb02e8ff10e42a1c8148743d
z64 <zachnowicki@gmail.com>
2016-09-04 16:15:24 +0000

    upgrade :draw to give you a single or combo idea

--- lib/modules/commands/draw.rb
+++ lib/modules/commands/draw.rb
@@ -5,9 +5,20 @@ module DrawBot
       extend Discordrb::Commands::CommandContainer
       command(:draw,
               description: 'Gives you a random idea of what to draw!',
-              usage: "#{BOT.prefix}draw") do |_event|
-        thing = Database::Response.where(key: 'draw').all.sample.response
-        "You should draw #{thing}"
+              usage: "#{BOT.prefix}draw") do |event|
+        single_or_combo = %w(single single single combo).sample
+        if single_or_combo == 'single'
+          thing = Database::Response.where(key: 'draw').all.sample.response
+          event << "You should draw #{thing}"
+        else
+          prefix = Database::Response.where(key: 'combo_prefix').all
+                                     .sample
+                                     .response
+          suffix = Database::Response.where(key: 'combo_suffix').all
+                                     .sample
+                                     .response
+          event << "You should draw something #{prefix} #{suffix}"
+        end
       end
     end
   end
