d3ac5199ebf0c1eaf2f00ee2ac48fac2f6619b69
z64 <zachnowicki@gmail.com>
2016-09-05 02:35:11 +0000

    add stats method to Bank

--- lib/modules/database/bank.rb
+++ lib/modules/database/bank.rb
@@ -8,6 +8,20 @@ module DrawBot
       def after_create
         Discordrb::LOGGER.info "created bank account: #{inspect}"
       end
+
+      # Bank stats
+      def stats
+        total = hearts + salt
+        percent_hearts = ((hearts.to_f / total.to_f) * 100).round(2)
+        percent_salt = ((salt.to_f / total.to_f) * 100).round(2)
+        {
+          total: total,
+          hearts: hearts,
+          salt: salt,
+          percent_hearts: percent_hearts,
+          percent_salt: percent_salt
+        }
+      end
     end
   end
 end
