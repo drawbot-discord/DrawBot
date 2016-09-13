7cfdefea79ca6b2a032eabb121d8e5923762cc43
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2016-09-13 01:31:03 -0400

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -58,6 +58,8 @@ end
 
 bot.ready do |event|
   event.bot.send_message(DEVCHANNEL, "Drawbot online! Let's get some art done!")
+  event.bot.servers.count
+  event.bot.servers.collect { |_, s| s.name }.join(', ')
   avatar = File.open('media/avatar.jpg','rb')
   event.bot.profile.avatar = avatar
   scheduler = Rufus::Scheduler.new
