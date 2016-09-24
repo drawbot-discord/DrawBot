d0597f64acc535f6c4ca29fd10e3b99f5fbf07b6
z64 <zachnowicki@gmail.com>
2016-09-24 18:30:03 -0400

    uncomment breaks

--- lib/modules/commands/drawfagcommands.rb
+++ lib/modules/commands/drawfagcommands.rb
@@ -7,13 +7,13 @@ module DrawBot
       extend Discordrb::Commands::CommandContainer
       command(:gimme,
               usage: "#{BOT.prefix}gimme") do |event|
-        break unless event.server.id == 175579371975868416
+        # break unless event.server.id == 175579371975868416
         'http://i.imgur.com/A9UWyst.png'
       end
 
       command(:doit,
               usage: "#{BOT.prefix}doit") do |event|
-        break unless event.server.id == 175579371975868416
+        # break unless event.server.id == 175579371975868416
         'http://i.imgur.com/nFWku9b.jpg'
       end
     end
