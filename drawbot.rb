ee5598ccccd10bb2910cc0552639cbc259e950bf
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2016-11-14 00:58:33 -0500

    ༼∩ •́ ヮ •̀ ༽⊃━☆ﾟ. * ･ ｡ﾟ x 2

--- drawbot.rb
+++ drawbot.rb
@@ -149,7 +149,7 @@ bot.message(with_text: '/o/') do |event|
 end
 
 
-bot.message(contains:/sparkle/i,/sparkles/i,/sparkling/i) do |event|
+bot.message(contains:/sparkle/i, /sparkles/i, /sparkling/i,) do |event|
   event.respond '༼∩ •́ ヮ •̀ ༽⊃━☆ﾟ. * ･ ｡ﾟ'
 
 end
