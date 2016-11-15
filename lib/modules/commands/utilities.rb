cacead76388e8c2d6325c688312bea64ac68b4bf
z64 <zachnowicki@gmail.com>
2016-11-15 03:34:37 -0500

    remove duplicate eval command

--- lib/modules/commands/utilities.rb
+++ lib/modules/commands/utilities.rb
@@ -9,18 +9,6 @@ module DrawBot
         'https://github.com/LeggoMyEcho/DrawBot/wiki/Commands'
       end
 
-      #------------Eval-----------#
-     command(:eval,
-                 help_available: false) do |event, *code|
-                  break unless event.user.id == 132893552102342656
-                  begin
-                  eval code.join(' ')
-                  rescue => e
-                  "An error occured, and I heard something\
-                        break :disappointed: ```#{e}```"
-                  end
-                end
-
      command (:getdb,
                    help_available: false) do |event|
                     break unless event.channel.id == DEVCHANNEL
