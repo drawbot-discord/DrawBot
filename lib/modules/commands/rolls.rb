62567182522d04e69c2efc1223c17a9644350ccc
z64 <zachnowicki@gmail.com>
2016-11-15 03:27:51 -0500

    fix :roll style

--- lib/modules/commands/rolls.rb
+++ lib/modules/commands/rolls.rb
@@ -1,16 +1,16 @@
-#Roll command
+# Roll command
 module DrawBot
   module DiscordCommands
     module Rolls
-     bot.command(:roll,
-                 description: 'Roll assorted dice!',
-                 usage: "#{BOT.prefix}roll") do |event, roll|
-     roll = roll.split('d').map(&:to_i)
-     roll = roll[0].times.collect { |x| rand(1..roll[1]) }
-     total = roll.inject(0){|sum,x| sum + x }
-     "#{event.user.display_name} throws their dice down\
-                and roll `#{roll.join(', ')} = #{total}`"
-     end                  
+      bot.command(:roll,
+                  description: 'Roll assorted dice!',
+                  usage: "#{BOT.prefix}roll NdS") do |event, roll|
+        roll = roll.split('d').map(&:to_i)
+        roll = Array.new(roll[0]) { rand(1..roll[1]) }
+        total = roll.inject(0) { |sum, x| sum + x }
+        "#{event.user.display_name} throws their dice down"\
+        "and roll `#{roll.join(', ')} = #{total}`"
+      end
     end
   end
 end
