4be5276f5aaf0e3bd1f62bc842be60d4c7165638
z64 <zachnowicki@gmail.com>
2016-11-15 03:35:26 -0500

    fix :getdb style

--- lib/modules/commands/utilities.rb
+++ lib/modules/commands/utilities.rb
@@ -9,12 +9,11 @@ module DrawBot
         'https://github.com/LeggoMyEcho/DrawBot/wiki/Commands'
       end
 
-     command (:getdb,
-                   help_available: false) do |event|
-                    break unless event.channel.id == DEVCHANNEL
-                    file = File.open('db.yaml')
-                    event.channel.send_file(file)
-                   end
+      command(:getdb, help_available: false) do |event|
+        break unless event.channel.id == DEVCHANNEL
+        file = File.open('db.yaml')
+        event.channel.send_file(file)
+      end
     end
   end
 end
