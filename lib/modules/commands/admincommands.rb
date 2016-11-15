3e14564fe82ddfc18e4cbc852a720ba6deacacd5
z64 <zachnowicki@gmail.com>
2016-11-15 03:22:06 -0500

    fix exceptions, style, extend CommandContainer

--- lib/modules/commands/admincommands.rb
+++ lib/modules/commands/admincommands.rb
@@ -1,24 +1,26 @@
 module DrawBot
   module DiscordCommands
+    # Commands for server and bot administration
     module AdminCommands
-      bot.command(:restart,
-                  description: 'restarts the bot',
-                  usage: "#{BOT.prefix}restart") do |event|
-       break unless event.channel.id == DEVCHANNEL
-       event.channel.send_message("Restart issued.. :wrench:\n
-                                          I'll be back soon!")
+      extend Discordrb::Commands::CommandContainer
+      command(:restart,
+              description: 'restarts the bot',
+              usage: "#{BOT.prefix}restart") do |event|
+        break unless event.channel.id == DEVCHANNEL
+        event.channel.send_message("Restart issued.. :wrench:\n"\
+                                   "I'll be back soon!")
         bot.stop
-       exit
+        exit
       end
 
-      bot.command(:bad
-                   description: 'Softbans a user',
-                   usage: "#{BOT.prefix}bad") do |event, *message|
-        break unless event.user.role? event.server.role { |r| ['DBAdmin','Moderator'].include? r.name }
+      command(:bad,
+              description: 'Softbans a user',
+              usage: "#{BOT.prefix}bad") do |event, *message|
+        break unless event.user.role? event.server.role { |r| %w(DBAdmin Moderator).include? r.name }
         unless event.message.mentions.nil?
-        softban = event.server.roles.find { |r| r.name == 'softban' }
-        event.message.mentions.first.on(event.server).roles = softban unless softban.nil?
-      end
+          softban = event.server.roles.find { |r| r.name == 'softban' }
+          event.message.mentions.first.on(event.server).roles = softban unless softban.nil?
+        end
         message = message.join(' ')
 
         "#{event.user.display_name} throws #{message} into timeout."
