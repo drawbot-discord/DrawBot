097a65624fa331c21b9351cbb1c36310289289da
z64 <zachnowicki@gmail.com>
2016-11-15 03:30:50 -0500

    remove use of bot.#

--- lib/modules/commands/utilities.rb
+++ lib/modules/commands/utilities.rb
@@ -3,13 +3,13 @@ module DrawBot
   module DiscordCommands
     module UtilityCommands
      extend Discordrb::Commands::CommandContainer
-     bot.command(:commands,
+     command(:commands,
                  description: 'Lists the publicly available commands',
                  usage: "#{BOT.prefix}commands") do |event|
      'https://github.com/LeggoMyEcho/DrawBot/wiki/Commands'
      end
 
-     bot.command(:restart,
+     command(:restart,
                  description: "restarts the bot") do |event|
                  break unless event.channel.id == DEVCHANNEL
                  event.channel.send_message("Restart issued.. :wrench:")
@@ -18,7 +18,7 @@ module DrawBot
       end
 
       #------------Eval-----------#
-     bot.command(:eval, 
+     command(:eval,
                  help_available: false) do |event, *code|
                   break unless event.user.id == 132893552102342656
                   begin
@@ -29,7 +29,7 @@ module DrawBot
                   end
                 end
 
-     bot.command (:getdb,
+     command (:getdb,
                    help_available: false) do |event|
                     break unless event.channel.id == DEVCHANNEL
                     file = File.open('db.yaml')
