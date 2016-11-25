f5ba2d5c432eab335b434d64d297aa4a061acffd
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2016-11-25 01:42:04 -0500

    lvl5 Lewd

--- drawbot.rb
+++ drawbot.rb
@@ -755,7 +755,8 @@ next event.respond 'Please give me something to search for' if search.nil?
 BASE_URL = 'https://e621.net/post/index/1/'
 e621 = Nokogiri::HTML RestClient.get(BASE_URL + search)
 pictures = e621.css('.thumb').map { |x| x.css('a').css('img').attr('src') }
-event << pictures.empty? ? 'couldn\'t find anything' : pictures.sample
+event.respond "`#{pictures}`"
+event.respond pictures.empty? ? 'couldn\'t find anything' : pictures.sample
 end
 
 bot.run
