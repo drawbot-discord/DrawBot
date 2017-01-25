0134a464ab647e9405bf12637e0f2b314553954e
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-01-25 01:01:32 -0500

    Fixed weird spacing

--- drawbot.rb
+++ drawbot.rb
@@ -804,8 +804,8 @@ bot.command(:submit,
      next event.respond('I need a link, hun!') unless /(http|https):\/\/(.)*/i.match(url.first)
    num = 8.times.map { rand(0...9) }.join
    url = url.join(' ')
-   event.bot.channel(215742813831168004).send_message("'Submission number #{num}'\n
-                            '#{event.user.display_name} posted their art' '#{url}'")
+   event.bot.channel(215742813831168004).send_message("'Submission number #{num}'
+   \n'#{event.user.display_name} posted their art' '#{url}'")
 end
 
 ############################
