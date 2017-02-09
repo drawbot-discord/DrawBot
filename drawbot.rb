b020ac8abf8c6988bfaf4381feb9e7770adbade6
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-02-09 03:15:00 -0500

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -128,13 +128,13 @@ bot.command(:drawlewd,
              usage: '~drawlewd') do |event|
    next event.respond "I need the `lewd` role for that, silly" unless
    event.bot.profile.on(event.server).roles.map {|x| x.name }.join.include? 'lewd'
-     if rand(1...10) == 1
+     if rand(1...5) == 1
        "You should draw #{event.user.display_name} #{LewdDrawTopic.sample}"
      else if rand(1...3) == 2
        "You should draw #{event.server.members.sample.display_name} #{LewdDrawTopic.sample}"
      else
        "You should draw #{DrawComboTopic.sample} #{LewdDrawTopic.sample}"
-     end
+     #end
      end
 end
 
