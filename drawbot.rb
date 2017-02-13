afa0427221bb9cab38afdc787240ae8c388331b6
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-02-13 01:19:44 -0500

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -1113,16 +1113,17 @@ bot.command(:debug) do |event|
     File.new("debug.txt", "w")
   end
 
+  {
+  debug: { long: 'DEBUG', short: 'D', format_code: '' },
+  good: { long: 'GOOD', short: '✓', format_code: "\u001B[32m" }, # green
+  info: { long: 'INFO', short: 'i', format_code: '' },
+  warn: { long: 'WARN', short: '!', format_code: "\u001B[33m" }, # yellow
+  error: { long: 'ERROR', short: '✗', format_code: "\u001B[31m" }, # red
+  out: { long: 'OUT', short: '→', format_code: "\u001B[36m" }, # cyan
+  in: { long: 'IN', short: '←', format_code: "\u001B[35m" } # purple
+}.freeze
   def initialize( fancy: false, stream: [STDOUT])
-    {
-    debug: { long: 'DEBUG', short: 'D', format_code: '' },
-    good: { long: 'GOOD', short: '✓', format_code: "\u001B[32m" }, # green
-    info: { long: 'INFO', short: 'i', format_code: '' },
-    warn: { long: 'WARN', short: '!', format_code: "\u001B[33m" }, # yellow
-    error: { long: 'ERROR', short: '✗', format_code: "\u001B[31m" }, # red
-    out: { long: 'OUT', short: '→', format_code: "\u001B[36m" }, # cyan
-    in: { long: 'IN', short: '←', format_code: "\u001B[35m" } # purple
-  }.freeze
+    mode :debug
   end
 
 file.write("debugtxt", "w")
