72c35e812864e4b72ff36919e2f236830af539a4
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-02-15 02:56:22 -0500

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -362,7 +362,7 @@ bot.command(:addchan,
                 file = File.open("serverlist.yaml", "w")
                 file.write($serverlist.to_yaml)
               end
-  channel = $serverlist['Allowedchans'][event.bot.channel(channel[2..-1])]
+  #channel = $serverlist['Allowedchans'][event.bot.channel(channel[2..-1])]
   server = $serverlist[Server][event.server.id]
       if server.nil?
       $serverlist['Server'][event.server.id] = Hash["Server" => event.server.id, "Name" => event.server.name, "Allowedchans" => [], "NSFW" => [] ]
