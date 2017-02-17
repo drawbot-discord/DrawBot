28aff5774347d674b3ecdffab8c5995034919143
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-02-17 05:34:18 -0500

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -801,7 +801,7 @@ bot.command(:e621, bucket: :e621, rate_limit_message: 'Calm down sweetheart! I c
         event.respond bigimage[1]
 end
 
-
+bot.bucket :r34, limit: 3, time_span: 30, delay: 10
 bot.command(:r34,
              description: "Search for an image on R34 Paheal",
              usage: `~r34 (search term)`) do |event, *search|
@@ -812,18 +812,17 @@ bot.command(:r34,
       next event.respond 'Please give me something to look for' if search.empty?
       #The URL the search starts with
       base_url = 'http://rule34.paheal.net/post/list/'
-      #Tacks the search onto the end of the base_url
+      #Tacks the search arg onto the end of the base_url
       rule34 = Nokogiri::HTML RestClient.get(base_url + search.join('_') + '/1')
       #Uses x to grab the URL from the css
       pictures = rule34.css('.shm-thumb').map do |x|
         #This produces the img URL from pictures
         x = x.css('a')[1].attr('href')
-        #x = "https://rule34.paheal.net#{x.css('a')[0].attr('href')}" # This gets the post not a direct image
+        #y = "https://rule34.paheal.net#{x.css('a')[0].attr('href')}" # This gets the post not a direct image
       end
       next event.respond 'No pictures found' if pictures.empty?
     rescue => error
       event.respond error
-      retry
     end
     #Spits out the randomized end result
     event.respond pictures.sample
