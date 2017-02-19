5ff938ac2072d6bdd6cf3be09c06788381671fab
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-02-19 03:57:06 -0500

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -143,8 +143,8 @@ bot.command(:info,
                value: "#{usw.uw_memused}", inline: true
     e.add_field name: 'Github',
                value: "[Click here](github.com/LeggoMyEcho/DrawBot)", inline:true
-    e.add_field name: "DrawBot Info",
-               value: "DrawBot .9 Alpha Ruby: `#{RUBY_VERSION}` Discordrb: `#{Discordrb::VERSION}`"              
+    e.add_field name: "\u200b",
+               value: "DrawBot .9 Alpha Ruby: `#{RUBY_VERSION}` Discordrb: `#{Discordrb::VERSION}`"
     e.footer = { text: "This operation took #{Time.now - event.timestamp} seconds to calculate" }
   end
 end
