680717c54dc11282f567bb51f4d44da1c9d362da
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-02-19 03:26:56 -0500

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -131,7 +131,7 @@ bot.command(:info,
     e.add_field name: 'TCP/UDP connections',
                value: "#{usw.uw_tcpused}/#{usw.uw_udpused}", inline: true
     e.add_field name: 'CPU used',
-               value: "#{usw.uw_cpuused}%", inline: false
+               value: "#{usw.uw_cpuused}%", inline: true
     e.add_field name: 'RAM usage',
                value: "#{usw.uw_memused}", inline: true
     e.add_field name: 'Disk space used',
