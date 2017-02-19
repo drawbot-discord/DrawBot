39490d8dbf836e822d809d89ef691a2622a97438
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-02-19 03:08:03 -0500

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -119,6 +119,10 @@ bot.command(:info,
               value: "`Echo#5248`\n"\
                       "`Lune#2639`\n"\
                       "`Cyan「Alter」#3717`", inline: true
+    e.add_field name: "Connected servers",
+               value:  "#{event.bot.servers.count}", inline: true
+    e.add_field name: "Connected users",
+               value: "#{event.bot.users.count}"
     e.add_field name: 'TCP/UDP connections',
                value: "#{usw.uw_tcpused}/#{usw.uw_udpused}", inline: true
     e.add_field name: 'Avg CPU lode (past 60 secs)',
