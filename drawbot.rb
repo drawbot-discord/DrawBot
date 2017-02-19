a6eadb5e00340d9dc072dd7c72a617fa6b7484c1
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-02-19 02:25:24 -0500

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -115,8 +115,8 @@ bot.command(:info,
                usw = Usagewatch
                event.channel.send_embed do |e|
     #e.description = '**DrawBot General information**'
-    e.add_field name: 'I am worked on by',
-               value: "`Echo#5248` and `Lune#2639` `Cyan「Alter」#3717`", inline: true
+  #  e.add_field name: 'I am worked on by',
+  #             value: "`Echo#5248` and `Lune#2639` `Cyan「Alter」#3717`", inline: true
     e.add_field name: "[my server](discord.gg/u3a2Ck9)", inline: true
     e.add_field name: "[Github link](github.com/LeggoMyEcho/DrawBot)", inline:true
     e.add_field name: "[Invite link](#{event.bot.invite_url})", inline: true
