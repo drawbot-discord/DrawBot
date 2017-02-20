8f42200f4e7c2bde95b395cb381bf9214025e3a3
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-02-20 00:41:42 -0500

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -391,12 +391,13 @@ bot.command(:reg) do |event|
             file = File.open("db.yaml", "w")
             file.write($db.to_yaml)
           end
-      
+      if user
+        "You're already registered sweetheart."
+      end
     if user.nil?
       $db['users'][event.user.id] = Hash["name" => event.user.display_name, "refs" => [], "hearts" => 0, "salt" => 0, "stipend" => 25]
       "User added! You now have `0 HEARTS`, `0 SALT`, `25 STIPEND`, and can add refs!"
     end
-
 end
 
 
