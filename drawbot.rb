7931f107ce5b158b6ad0ed8b5ef1093ac6ed02e1
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-02-25 23:43:36 -0500

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -251,17 +251,17 @@ end
 bot.command([:randomchar, :pc],
              description: "Generate a random fantasy character (Pathfinder/DnD)",
              usage: '~randomchar') do |event|
-  Gender = ["He", "She"].sample
+  gender = ["He", "She"].sample
   ##HairLength = ["long", "short", "thick", "thin"]
   ##Height = ["is shorter than most of their kind", "is taller than most of their kind", "is of average height"]
   event << "Your randomly generated fantasy character is a;"
   event << " "
   event << "#{Align.sample} #{Race.sample} #{PClass.sample}, #{Stats.sample}"
-  event << "#{Gender} may follow `#{Deity.sample}`, `#{Deity.sample}`,"\
+  event << "#{gender} may follow `#{Deity.sample}`, `#{Deity.sample}`,"\
                                  " `#{Deity.sample}`, or `#{Deity.sample}`"
   event << "Possible names are `#{FantasyNames.sample}` `#{FantasyNames.sample}`"\
                                " `#{FantasyNames.sample}` `#{FantasyNames.sample}`"
-  event << "#{Gender} has #{HairLength.sample} #{Hair.sample} hair, #{BodyType.sample}"\
+  event << "#{gender} has #{HairLength.sample} #{Hair.sample} hair, #{BodyType.sample}"\
            " body and #{Height.sample}."
 end
 
