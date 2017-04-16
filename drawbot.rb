f3fbe94a60894951b17a47cc5dbd23d56b9b2599
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-04-16 00:03:08 -0400

    Add files via upload

--- drawbot.rb
+++ drawbot.rb
@@ -426,7 +426,8 @@ bot.command(:references,
     ref = $db['refs'].find { |r| r['title'].casecmp(args).zero? }
     unless ref.nil?
       event.channel.send_embed do |e|
-      e.add_field name: "#{ref['title']}", value:"#{ref['url']}", inline: true
+      e.add_field name: "#{ref['title']}", value: "\u200b", inline: true
+      e.image       = { url: "#{ref['url']}" }
       end
 
       return
