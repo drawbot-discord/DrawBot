3b3a3f3f34595eaf4ebf327ab6d76adafb7605fc
LeggoMyEcho <LeggoMyEcho@Hotmail.com>
2017-11-03 06:05:41 -0400

    changed the search commands around

--- src/modules/commands/search.rb
+++ src/modules/commands/search.rb
@@ -7,29 +7,21 @@ module Bot
       bucket :e621, limit: 3, time_span: 30, delay: 10
       command(:e621, bucket: :e621, rate_limit_message: 'Calm down sweetheart! I can\'t keep up with the lewd!',
               description: "Search for an image on e621.net",
-              usage: '~e621 (search_term)') do |event, *search|
-              next event.respond "I'm sorry. I can't do that because this is a SFW channel." unless event.channel.nsfw?
-              check = event.bot.profile.on(event.server).roles.map {|x| x.name }  & ["e621", "unlocknsfw"]
-              next event.respond "I need the `e621` or `unlocknsfw` role for that, silly" if check.empty?
-              searchterm = search.join(" ")
-              search = search.join('%20')
-
-              next event.respond 'Please give me something to search for' if search.empty?
-                base_url = 'https://e621.net/post/index/1/'
-                e621 = Nokogiri::HTML RestClient.get(base_url + search)
-                pictures = e621.css('.thumb').map do |x|
-                x = "https://e621.net#{x.css('a').attr('href')}"
-                end
-              next event.respond 'I couldn\'t find anything, sorry hun.' if pictures.empty?
-                bigimage_page = Nokogiri::HTML RestClient.get(pictures.sample)
-                bigimage = bigimage_page.css('.content').css('img').map do |x|
-                  x.attr('src')
-              end
-              event.channel.send_embed do |e|
-              e.image  = { url: bigimage[1] }
-              e.description = "Search result for: `#{searchterm}`"
-          end
-          event.message.delete
+              usage: '~e621 (search_term)') do |event, *tags|
+        next event.respond "I'm sorry. I can't do that because this is a SFW channel." unless event.channel.nsfw?
+        check = event.bot.profile.on(event.server).roles.map {|x| x.name }  & ["e621", "unlocknsfw"]
+        next event.respond "I need the `e621` or `unlocknsfw` role for that, silly" if check.empty?
+        tags = tags.join(" ")
+        next "Give me something to search for" if tags.empty?
+        result = RestClient.get("https://e621.net/post/index.json?tags=#{tags}")
+        json = JSON.parse(result).sample
+          next event.respond 'I couldn\'t find anything, sorry hun.' if json.nil?
+        event.channel.send_embed do |e|
+          e.add_field name: '__**Artist**__', value: json['artist'][0] , inline: true
+          e.add_field name: "\u200b", value: "[Source](https://e621.net/post/show/#{json['file_url']})", inline: true
+          e.image = { url: json['file_url'] }
+          e.description = "Search result for: `#{tags}`"
+        end
       end
 
       bucket :paheal, limit: 3, time_span: 30, delay: 10
@@ -62,6 +54,29 @@ module Bot
               rescue
                 event.respond "Couldn't find anything"
               end
+      end
+
+
+      bucket :paheal, limit: 3, time_span: 30, delay: 10
+      command(:r34,
+              description: "Search for an image on R34 Paheal",
+              usage: `~r34 (search term)`) do |event, *tags|
+                next event.respond "I'm sorry. I can't do that because this is a SFW channel." unless event.channel.nsfw?
+                check = event.bot.profile.on(event.server).roles.map {|x| x.name }  & ["r34", "unlocknsfw"]
+                next event.respond "I need the `e621` or `unlocknsfw` role for that, silly" if check.empty?
+                tags = tags.join(" ")
+                next "Give me something to search for" if tags.empty?
+                result = RestClient.get("https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=#{tags}")
+                xml = XmlSimple.xml_in result
+                postcount = xml['count'].to_i
+                  next "I couldn\'t find anything, sorry hun." if postcount == 0
+                post = xml['post'].sample
+
+                event.channel.send_embed do |e|
+                  e.add_field name: "\u200b", value: "[Source](https://rule34.xxx/index.php?page=post&s=view&id=#{post['id']})", inline: true
+                  e.image = { url: "https:#{post['file_url']}" }
+                  e.description = "Search result for: `#{tags}`"
+                end
       end
     end
   end
