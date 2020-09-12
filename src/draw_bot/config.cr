a9258ccd5c8652861bbebf5e1f716da2eefb8d6a
PixeL <officialpixelinc@gmail.com>
2020-09-12 04:06:50 -0500

    Add suggest command
    This adds the suggest command with a little different formatting.
    It also adds a new config value `suggestions_channel`.

--- src/draw_bot/config.cr
+++ src/draw_bot/config.cr
@@ -7,11 +7,13 @@ module DrawBot
     property token : String
     property owner_id : UInt64
     property client_id : UInt64
+    property suggestions_channel : UInt64
 
     def initialize(
       @token : String,
       @owner_id : UInt64,
-      @client_id : UInt64
+      @client_id : UInt64,
+      @suggestions_channel: UInt64,
     )
     end
   end
