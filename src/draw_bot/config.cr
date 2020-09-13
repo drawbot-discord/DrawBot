3ed5aab88cc131ab5adc5eee5187528af2fca2a7
PixeL <officialpixelinc@gmail.com>
2020-09-13 03:15:35 -0500

    Add suggest command (#32)
    * Add suggest command
    * Put server invite in config and use in new command
    
    This adds the suggest command with a little different formatting.
    It also adds a new config value `suggestions_channel` and `support_server_invite`.

--- src/draw_bot/config.cr
+++ src/draw_bot/config.cr
@@ -7,11 +7,15 @@ module DrawBot
     property token : String
     property owner_id : UInt64
     property client_id : UInt64
+    property suggestions_channel : UInt64
+    property support_server_invite : String
 
     def initialize(
       @token : String,
       @owner_id : UInt64,
-      @client_id : UInt64
+      @client_id : UInt64,
+      @suggestions_channel: UInt64,
+      @support_server_invite : String
     )
     end
   end
---
+++ src/draw_bot/handlers/suggest.cr
@@ -0,0 +1,47 @@
+module DrawBot
+  client.on_message_create(
+    SplitParser.new("~suggest", join_after: 1, min_args: 1)
+  ) do |payload, context|
+    message = context[SplitParser::Results].arguments.join(' ')
+
+    channel = cache.resolve_channel(payload.channel_id)
+    guild_name = if guild_id = payload.guild_id
+                   cache.resolve_guild(guild_id).name
+                 else
+                   "DM"
+                 end
+
+    created_in = <<-CREATED
+    Server: **#{guild_name}**
+    Channel: **#{channel.name || payload.channel_id}**
+    User: **#{payload.author.username}##{payload.author.discriminator} (#{payload.author.id})**
+    CREATED
+
+    embed = Discord::Embed.new(
+      title: "New Suggestion",
+      description: message,
+      fields: [
+        Discord::EmbedField.new(
+          name: "__**Created In**__",
+          value: created_in
+        ),
+      ],
+      footer: Discord::EmbedFooter.new(text: "Ref Num: #{payload.id}")
+    )
+
+    client.create_message(
+      config.suggestions_channel,
+      "",
+      embed
+    )
+    client.create_message(
+      payload.channel_id,
+      "",
+      Discord::Embed.new(
+        title: "I went ahead and submitted your suggestion!",
+        description: "[Be sure to join my development server to discuss your idea.](#{config.support_server_invite})",
+        colour: 0x08FF1F,
+      )
+    )
+  end
+end
